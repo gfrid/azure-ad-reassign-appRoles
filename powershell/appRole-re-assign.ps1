b5f2cc213be013980bfcd4f5d31bff570bbe80ab
George <97164735+gfrid@users.noreply.github.com>
2022-04-28 06:30:11 +0300

    Create appRole-re-assign.ps1

---
+++ powershell/appRole-re-assign.ps1
@@ -0,0 +1,64 @@
+$groups = get-azadgroup -DisplayNameStartsWith "AWS-" | Select DisplayName, Description, id
+foreach ($d in $groups) {
+   write-host $d.DisplayName
+   $d.DisplayName = $d.DisplayName -replace "AWS", "+AWS" 
+   #checking new name to validate its changed in replace
+   $d.DisplayName
+   $d.Description
+   #New-AzureADGroup -DisplayName $d.DisplayName -MailEnabled $false -SecurityEnabled $true -MailNickName "NotSet" -Description $d.Description
+   }
+
+#REMOVE APPROLE ASSIGN
+
+$groups = get-azadgroup -DisplayNameStartsWith "AWS-" | Select DisplayName, Description, id
+foreach ($i in $groups) {
+
+  $assign = Get-AzureADGroupAppRoleAssignment -ObjectId $i.id 
+       foreach ($text in $assign.PrincipalDisplayName) {
+            $separator = "-"
+            $parts = $text.split($separator)
+            $acc = $parts[1]
+            $role = $parts[2]
+            $acc, $role
+            $app_name = "AWS-x Single Sign-on"
+            $sp = Get-AzureADServicePrincipal -Filter "displayName eq '$app_name'"
+            foreach ($a in $acc) {
+              foreach ($r in $role) {
+                $appRole = $sp.AppRoles | Where-Object { $_.DisplayName -match "\b$role\b" -and $_.DisplayName -match $acc } 
+                write-host $appRole.DisplayName $appRole.id $assign.PrincipalDisplayName -background green
+                #Remove-AzureADGroupAppRoleAssignment -ObjectId $i.id -AppRoleAssignmentId 
+             }
+            }
+         }
+         }
+
+#ADD NEW APPROLE ASSIGN
+
+$groups = get-azadgroup -DisplayNameStartsWith "+AWS-" | Select DisplayName, Description, id
+foreach ($i in $groups) {
+
+  $assign = Get-AzureADGroupAppRoleAssignment -ObjectId $i.id 
+       foreach ($text in $assign.PrincipalDisplayName) {
+            $separator = "-"
+            $parts = $text.split($separator)
+            $acc = $parts[1]
+            $role = $parts[2]
+            $acc, $role
+            $app_name = "AWS-x Single Sign-on"
+            $sp = Get-AzureADServicePrincipal -Filter "displayName eq '$app_name'"
+            foreach ($a in $acc) {
+              foreach ($r in $role) {
+                $appRole = $sp.AppRoles | Where-Object { $_.DisplayName -match "\b$role\b" -and $_.DisplayName -match $acc } 
+                write-host $appRole.DisplayName $appRole.id $assign.PrincipalDisplayName -background green
+                #New-AzureADGroupAppRoleAssignment -ObjectId $i.id  -PrincipalId $i.id -ResourceId $sp.ObjectId -Id $appRole.Id
+             }
+            }
+         }
+         }
+
+#Some TESTS
+
+ $groups = get-azadgroup -DisplayNameStartsWith "AWS-" | Select DisplayName, Description, id
+    foreach ($i in $groups) {
+    Get-AzureADGroupAppRoleAssignment -ObjectId $i.id 
+}
